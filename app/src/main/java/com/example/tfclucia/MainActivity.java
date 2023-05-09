8278a70a794f13abd0cc705e848358619fa3afdc
Your Name <you@example.com>
2023-05-10 00:34:39 +0200

    Splash

--- app/src/main/java/com/example/tfclucia/MainActivity.java
+++ app/src/main/java/com/example/tfclucia/MainActivity.java
@@ -2,13 +2,39 @@ package com.example.tfclucia;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        Animation animation1 = AnimationUtils.loadAnimation(this, R.anim.desplazamiento_arriba);
+        Animation animation2 = AnimationUtils.loadAnimation(this, R.anim.desplazamiento_abajo);
+
+        TextView titleTextView = findViewById(R.id.titleTextView);
+        ImageView logoImageView = findViewById(R.id.logoImageView);
+
+        titleTextView.setAnimation(animation2);
+        logoImageView.setAnimation(animation1);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 4000);
     }
 }
