ed5372f99cdca7fc15838244d7d877669f139d17
Your Name <you@example.com>
2023-05-19 23:48:58 +0200

    .

--- app/src/main/java/com/example/tfclucia/LoginActivity.java
+++ app/src/main/java/com/example/tfclucia/LoginActivity.java
@@ -1,17 +1,31 @@
 package com.example.tfclucia;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ActivityOptions;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import kotlin.Pair;
 
@@ -19,7 +33,9 @@ public class LoginActivity extends AppCompatActivity {
 
     TextView bienvenidoLabel, continuarLabel, nuevoUsuario;
     ImageView loginImageView;
-    TextInputLayout usuarioTextField, contrasenaTextField;
+    EditText usuarioTextField, contrasenaTextField;
+    String emailString, contrasenaString;
+    String url = "http://169.254.104.165/ws1/login.php";
     MaterialButton inicioSesion;
 
     @Override
@@ -58,5 +74,50 @@ public class LoginActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+    public void login (View view){
+        if (usuarioTextField.getText().toString().equals("")){
+            Toast.makeText(this, "ingresa un correo", Toast.LENGTH_SHORT).show();
+        }else if (contrasenaTextField.getText().toString().equals("")){
+            Toast.makeText(this, "ingresa una contrasena", Toast.LENGTH_SHORT).show();
+        } else {
+            final ProgressDialog progressDialog =new ProgressDialog(this);
+            progressDialog.setMessage("Cargando");
+            progressDialog.show();
+
+            emailString = usuarioTextField.getText().toString().trim();
+            contrasenaString = contrasenaTextField.getText().toString().trim();
+
+            StringRequest request = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    progressDialog.dismiss();
+                    if (response.equalsIgnoreCase("ingreso correcto")) {
+                        usuarioTextField.setText("");
+                        contrasenaTextField.setText("");
+                        startActivity(new Intent(getApplicationContext(), RegisterActivity.class));
+                    } else {
+                        Toast.makeText(LoginActivity.this, response, Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    progressDialog.dismiss();
+                    Toast.makeText(LoginActivity.this, error.getMessage().toString(), Toast.LENGTH_SHORT).show();
+                }
+            }){
+                @Nullable
+                @Override
+                protected Map<String, String> getParams() throws AuthFailureError {
+                    Map<String,String> params = new HashMap<>();
+                    params.put("email", emailString);
+                    params.put("contrasena", contrasenaString);
+                    return params;
+                }
+            };
+            RequestQueue requestQueue = Volley.newRequestQueue(LoginActivity.this);
+            requestQueue.add(request);
+        }
     }
 }
