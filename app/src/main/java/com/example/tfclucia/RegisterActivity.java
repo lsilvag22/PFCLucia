419780d97e7d8f5f97518a56ed92373362c3a507
Your Name <you@example.com>
2023-05-22 00:26:05 +0200

    register con firebase

--- app/src/main/java/com/example/tfclucia/RegisterActivity.java
+++ app/src/main/java/com/example/tfclucia/RegisterActivity.java
@@ -1,14 +1,19 @@
 package com.example.tfclucia;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ActivityOptions;
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Build;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -21,8 +26,16 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -34,12 +47,18 @@ public class RegisterActivity extends AppCompatActivity {
     TextView nuevoUsuario, bienvenidoLabel, continuarLabel;
     ImageView registerImageView;
     EditText nameTextField, usuarioRegisterTextField, contrasenaTextField;
-    MaterialButton registro;
+    Button registro;
+    FirebaseFirestore mFirestore;
+    FirebaseAuth mAuth;
+    String userId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        mFirestore = FirebaseFirestore.getInstance();
+        mAuth = FirebaseAuth.getInstance();
 
         registerImageView = findViewById(R.id.registerImageView);
         bienvenidoLabel = findViewById(R.id.bienvenidoLabel);
@@ -55,67 +74,54 @@ public class RegisterActivity extends AppCompatActivity {
             public void onClick(View view) {
 
                 transitionBack();
-                insertarDatos();
+                createUser();
             }
         });
 
 
     }
+    public void createUser(){
+        String nombre = nameTextField.getText().toString().trim();
+        String email = usuarioRegisterTextField.getText().toString().trim();
+        String contrasena = contrasenaTextField.getText().toString().trim();
 
-    private void insertarDatos() {
-        final String nombre = nameTextField.getText().toString().trim();
-        final String email = usuarioRegisterTextField.getText().toString().trim();
-        final String contrasena = contrasenaTextField.getText().toString().trim();
-
-        final ProgressDialog progressDialog = new ProgressDialog(this);
-        progressDialog.setMessage("Cargando");
-
-        if (nombre.isEmpty()){
+        if (TextUtils.isEmpty(nombre)){
             Toast.makeText(this, "Ingresa un nombre", Toast.LENGTH_SHORT).show();
-            return;
-        } else if (email.isEmpty()){
+            nameTextField.requestFocus();
+        } else if (TextUtils.isEmpty(email)){
             Toast.makeText(this, "Ingresa un correo", Toast.LENGTH_SHORT).show();
-            return;
-        } else if (contrasena.isEmpty()){
+            usuarioRegisterTextField.requestFocus();
+        } else if (TextUtils.isEmpty(contrasena)){
             Toast.makeText(this, "Ingresa una contraseña", Toast.LENGTH_SHORT).show();
-            return;
+            contrasenaTextField.requestFocus();
         } else {
-            progressDialog.show();
-            StringRequest request = new StringRequest(Request.Method.POST, "http://169.254.104.165/ws1/index.php", new Response.Listener<String>() {
-                @Override
-                public void onResponse(String response) {
-                    if (response.equalsIgnoreCase("datos insertados")) {
-                        Toast.makeText(RegisterActivity.this, "datos insertados", Toast.LENGTH_SHORT).show();
-                        progressDialog.dismiss();
 
-                        startActivity(new Intent(getApplicationContext(), LoginActivity.class));
-                        finish();
+            mAuth.createUserWithEmailAndPassword(email,contrasena).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        userId = mAuth.getCurrentUser().getUid();
+                        DocumentReference documentReference = mFirestore.collection("users").document(userId);
+
+                        Map<String, Object> params = new HashMap<>();
+                        params.put("id", userId);
+                        params.put("nombre", nombre);
+                        params.put("email", email);
+                        params.put("contrasena", contrasena);
+
+                        documentReference.set(params).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void unused) {
+                                Log.d("TAG", "onSuccess: Datos registrados" + userId);
+                            }
+                        });
+                        Toast.makeText(RegisterActivity.this, "Usuario registrado", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
                     } else {
-                        Toast.makeText(RegisterActivity.this, response, Toast.LENGTH_SHORT).show();
-                        progressDialog.dismiss();
+                        Toast.makeText(RegisterActivity.this, "Usuario no registrado" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                     }
                 }
-            }, new Response.ErrorListener() {
-                @Override
-                public void onErrorResponse(VolleyError error) {
-                    Toast.makeText(RegisterActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
-                    progressDialog.dismiss();
-                }
-            }){
-                @Nullable
-                @Override
-                protected Map<String, String> getParams() throws AuthFailureError {
-
-                    Map<String, String> params = new HashMap<>();
-                    params.put("nombre", nombre);
-                    params.put("email", email);
-                    params.put("contrasena", contrasena);
-                    return params;
-                }
-            };
-
-            RequestQueue requestQueue = Volley.newRequestQueue(RegisterActivity.this);
-            requestQueue.add(request);
+            });
         }
     }
 
